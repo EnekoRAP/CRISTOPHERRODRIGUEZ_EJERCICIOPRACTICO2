269a7294250ea03c19802cf749a84341a9cbc38d
Cristopher Rodríguez <145072878+trebleprodigy@users.noreply.github.com>
2024-04-17 22:17:10 -0600

    Otro controller*

--- Hotel/src/main/java/com/Prueba2/controller/UsuariosController.java
+++ Hotel/src/main/java/com/Prueba2/controller/UsuariosController.java
@@ -8,50 +8,47 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Controller
-@RequestMapping("/usuarios")
+@RequestMapping("/usuario")
 public class UsuariosController {
-
-    @Autowired
-    private UsuariosService usuariosService;
+    
+     @Autowired
+    private UsuariosService usuarioService;
 
     @GetMapping("/listado")
     public String listado(Model model) {
-        var usuarios = usuariosService.getUsuarios();
+        var usuarios = usuarioService.getUsuarios();
         model.addAttribute("usuarios", usuarios);
         model.addAttribute("totalUsuarios", usuarios.size());
-        return "/usuarios/listado";
+        return "/usuario/listado";
     }
 
     @GetMapping("/nuevo")
-    public String usuarioNuevo(Usuarios usuarios) {
-        return "/usuarios/modifica";
+    public String usuarioNuevo(Usuarios usuario) {
+        return "/usuario/modifica";
     }
 
     @PostMapping("/guardar")
-    public String usuarioGuardar(Usuarios usuarios,
-            @RequestParam("correo") String correo) {
-        if (correo != null && !correo.isEmpty()) {
-            usuarios.setCorreo(correo);
-            usuariosService.save(usuarios, false);
-        }
-        usuariosService.save(usuarios, true);
-        return "redirect:/usuarios/listado";
+    public String usuarioGuardar(Usuarios usuario) {
+        var codigo = new BCryptPasswordEncoder();
+        usuario.setPassword(codigo.encode(usuario.getPassword()));
+        usuarioService.save2(usuario,1);
+        return "redirect:/usuario/listado";
     }
 
     @GetMapping("/eliminar/{idUsuario}")
-    public String usuarioEliminar(Usuarios usuarios) {
-        usuariosService.delete(usuarios);
+    public String usuarioEliminar(Usuarios usuario) {
+        usuarioService.delete(usuario);
         return "redirect:/usuario/listado";
     }
 
     @GetMapping("/modificar/{idUsuario}")
-    public String usuarioModificar(Usuarios usuarios, Model model) {
-        usuarios = usuariosService.getUsuario(usuarios);
-        model.addAttribute("usuarios", usuarios);
-        return "/usuarios/modifica";
+    public String usuarioModificar(Usuarios usuario, Model model) {
+        usuario = usuarioService.getUsuario(usuario);
+        model.addAttribute("usuario", usuario);
+        return "/usuario/modifica";
     }
-    
 }
+
